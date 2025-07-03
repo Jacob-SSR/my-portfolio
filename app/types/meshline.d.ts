2006d6b215a110ca35546dd688cdc874ece90816
Iam-Jacobbbbbbb <teepakorn.snis@gmail.com>
2025-07-03 23:29:32 +0700

    fix and deploys

---
+++ app/types/meshline.d.ts
@@ -0,0 +1,18 @@
+import "react";
+
+declare module "react" {
+  namespace JSX {
+    interface IntrinsicElements {
+      meshLineGeometry: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      meshLineMaterial: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+        color?: string;
+        depthTest?: boolean;
+        resolution?: [number, number];
+        useMap?: boolean;
+        map?: THREE.Texture;
+        repeat?: [number, number];
+        lineWidth?: number;
+      };
+    }
+  }
+}
